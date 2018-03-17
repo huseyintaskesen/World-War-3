b2106ccdb329c98a4e02dfb11eba21104e69127b
ibrahimcavdar <cavdarhibrahim@gmail.com>
2018-03-17 12:02:38 +0300

    the menu
    menu and transition between states are done using Slick2D library

---
+++ src/game/Credits.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package game;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ * @author ibrahim
+ *
+ */
+public class Credits extends BasicGameState {
+
+	Image view;
+	public String mouse= "No input yet";
+	
+	public Credits(int state) {
+
+	}
+
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		view = new Image("res/credits.png");
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		view.draw(0, 0);
+		
+//		g.fillOval(75, 100, 100, 100);
+//		g.drawString("Play", 80, 70);
+		g.setColor(Color.white);
+		g.fillRect(300, 300, 150, 30);
+		g.setColor(Color.black);
+		g.drawString(mouse, 300, 300);
+		
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		Input input = gc.getInput();
+		int xpos = Mouse.getX();
+		int ypos = 600 - Mouse.getY();
+		mouse = "x : "+xpos+" y : "+ypos;
+		if ((10 < xpos && xpos < 400) && (25 < ypos && ypos < 95)) {
+			if (input.isMouseButtonDown(0)) {
+				sbg.enterState(0);
+			}
+		}
+	}
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
+
+}
---
+++ src/game/Play.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package game;
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+/**
+ * @author ibrahim
+ *
+ */
+public class Play extends BasicGameState{
+	
+	public String mouse= "No input yet";
+	Image view;
+	
+	public Play (int state) {
+		
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+		view = new Image("res/play.png");
+	}
+	
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		view.draw(0, 0);
+		g.drawString("Play State", 200, 200);
+		g.drawRect(50, 50, 100, 50);
+		g.drawString("Back", 60, 67);
+		
+		g.setColor(Color.white);
+		g.fillRect(300, 300, 150, 30);
+		g.setColor(Color.black);
+		g.drawString(mouse, 300, 300);
+	}
+	public void update(GameContainer gc, StateBasedGame sbg,int delta) throws SlickException {
+		Input input = gc.getInput();
+		int xpos = Mouse.getX();
+		int ypos = 600 - Mouse.getY();
+		mouse = "x : "+xpos+" y : "+ypos;
+		if ((50 < xpos && xpos < 150) && (50 < ypos && ypos < 100)) {
+			if (input.isMouseButtonDown(0)) {
+				sbg.enterState(0);
+			}
+		}
+		if ((750 < xpos && xpos < 790) && (19 < ypos && ypos < 72)) {
+			if (input.isMouseButtonDown(0)) {
+				sbg.enterState(0);
+			}
+		}
+	}
+	public int getID() {
+		return 1;
+	}
+}
